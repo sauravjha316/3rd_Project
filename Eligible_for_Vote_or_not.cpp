39d27a4834db42e61116db6a22f7d83a58e18e69
sauravjha316 <69289283+sauravjha316@users.noreply.github.com>
2020-08-16 21:03:28 +0530

    Eligible_for_Vote_or_not
    A c++ program to check a person is eligible for vote or not by entering their age

---
+++ Eligible_for_Vote_or_not.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+int age;
+cout<<"Enter the age of a person   ";
+cin>>age;
+if(age>=18)
+{
+cout<<"Person is Eligible for vote";
+}
+else
+{
+cout<<"Not eligible for vote";
+}
+return 0;
+}
